985644732bc85d542c037dd0f77959a305fa2b38
JesusAlfonsoRojasRocha <jr6956109@gmail.com>
2026-04-12 22:50:40 -0400

    Bionex

---
+++ Js/globales.js
@@ -0,0 +1,6 @@
+const btnSubir = document.getElementById('btnSubir');
+
+btnSubir.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
--- Mi_Empresa/Js/globales.js
+++
@@ -1,6 +0,0 @@
-const btnSubir = document.getElementById('btnSubir');
-
-btnSubir.onclick = function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
